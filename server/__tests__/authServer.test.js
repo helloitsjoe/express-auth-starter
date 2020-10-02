6183daa7fc51cde1334c33b4b2418f5c0ee3497c
helloitsjoe <jboyle617@gmail.com>
2020-10-02 06:35:37 -0400

    Add tests and signup

--- server/__tests__/authServer.test.js
+++ server/__tests__/authServer.test.js
@@ -26,9 +26,81 @@ test('returns server if already listening', async () => {
   expect(listeningServer).toBe(server);
 });
 
-test('oauth route returns oauth dialog', async () => {
-  const res = await axios.get(`${rootUrl}/oauth`);
-  const dataIsHTML = /<!DOCTYPE html>/.test(res.data);
-  expect(dataIsHTML).toBe(true);
-  expect(res.data).toMatch(/<button(.*)>Authorize<\/button>/i);
+describe('oauth', () => {
+  it('oauth route returns oauth dialog', async () => {
+    const res = await axios.get(`${rootUrl}/oauth`);
+    const dataIsHTML = /<!DOCTYPE html>/.test(res.data);
+    expect(dataIsHTML).toBe(true);
+    expect(res.data).toMatch(/<button(.*)>Authorize<\/button>/i);
+  });
+});
+
+describe('jwt', () => {
+  describe('/signup', () => {
+    it('returns token', async () => {
+      const res = await axios.post(`${rootUrl}/jwt/signup`, {
+        username: 'foo',
+        password: 'bar',
+      });
+      expect(res.data.token).toMatch(/\w+.\w+.\w+/);
+    });
+
+    it('returns error if username exists', async () => {
+      expect.assertions(1);
+      await axios.post(`${rootUrl}/jwt/signup`, {
+        username: 'first',
+        password: 'bar',
+      });
+
+      return axios
+        .post(`${rootUrl}/jwt/signup`, {
+          username: 'first',
+          password: 'bar',
+        })
+        .catch(err => {
+          expect(err.response.data.message).toMatch(/unavailable/i);
+        });
+    });
+  });
+
+  describe('/login', () => {
+    it('returns token', async () => {
+      // TODO: Seed data and isolate tests
+      const res = await axios.post(`${rootUrl}/jwt/login`, {
+        username: 'foo',
+        password: 'bar',
+      });
+      expect(res.data.token).toMatch(/\w+.\w+.\w+/);
+    });
+
+    it('returns error if user does not exist', () => {
+      expect.assertions(1);
+      return axios
+        .post(`${rootUrl}/jwt/login`, {
+          username: 'nobody',
+          password: 'bar',
+        })
+        .catch(err => {
+          expect(err.response.data.message).toMatch(/does not exist/i);
+        });
+    });
+
+    it('returns error if password does not match', () => {
+      expect.assertions(1);
+      return axios
+        .post(`${rootUrl}/jwt/login`, {
+          username: 'foo',
+          password: 'wrong-password',
+        })
+        .catch(err => {
+          expect(err.response.data.message).toMatch(/wrong password/i);
+        });
+    });
+  });
+
+  describe('/secure', () => {
+    it.todo('returns error if no token');
+    it.todo('returns error if invalid token');
+    it.todo('returns response if valid token');
+  });
 });
