cf7df1d6aa97577f82d0046711a876ce31f482cd
helloitsjoe <jboyle617@gmail.com>
2020-10-18 15:56:27 -0400

    generalize makeDbClient

--- server/index.js
+++ server/index.js
@@ -1,18 +1,12 @@
-const { MongoClient } = require('mongodb');
 const makeServer = require('./makeServer');
 const makeAuthServer = require('./makeAuthServer');
-const { makeCollection } = require('./services');
+const { makeCollection, makeDbClient } = require('./services');
 
-const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
-
-MongoClient.connect(`mongodb://${MONGO_HOST}:27017/auth`, { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to DB!');
-
-    const db = client.db('auth');
-
+makeDbClient(process.env.DB_URL)
+  .then(connection => {
+    console.log('Connected to Mongo');
     makeServer(3000);
-    makeAuthServer(3001, { users: makeCollection(db.collection('users')) });
+    makeAuthServer(3001, { users: makeCollection(connection.db('auth').collection('users')) });
   })
   .catch(err => {
     console.error('Error connecting to DB:', err);
