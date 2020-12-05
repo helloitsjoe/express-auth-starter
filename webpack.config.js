c9553369a95d92611a9a5492d5ff3a23a43b5197
helloitsjoe <jboyle617@gmail.com>
2020-12-04 23:09:11 -0500

    Update to TS webpack-simple

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,11 @@ const path = require('path');
 const { makeWebpackConfig } = require('webpack-simple');
 
 const config = makeWebpackConfig({
+  ts: true,
   entry: './src/index.tsx',
   output: {
     path: path.join(__dirname, '/public'),
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-  },
-});
-
-config.module.rules.push({
-  test: /\.tsx?$/,
-  use: { loader: 'ts-loader' },
 });
 
 module.exports = config;
